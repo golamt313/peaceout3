3a007222584019b7f42b431f1003a1b68cc6f85c
juizohora <fv2279@wayne.edu>
2021-11-03 19:48:10 -0400

    edittest

---
+++ App/app/src/test/java/com/example/splash/MainActivityTest.java
@@ -0,0 +1,22 @@
+package com.example.splash;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class MainActivityTest {
+
+    @Before
+    public void setUp() {
+    }
+    @After
+    public void tearDown() {
+
+    }
+    @Test
+    public void Test1(){
+        Assert.assertTrue(true);
+    }
+}
