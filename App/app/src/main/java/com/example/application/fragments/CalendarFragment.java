61585660e8cb6b98ad8cebfae19982c94cf73f1b
JosephThomas2001 <91638891+JosephThomas2001@users.noreply.github.com>
2021-11-19 12:41:22 -0500

    edited to resolve login issue

--- App/app/src/main/java/com/example/application/fragments/CalendarFragment.java
+++ App/app/src/main/java/com/example/application/fragments/CalendarFragment.java
@@ -1,20 +1,56 @@
 package com.example.application.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.application.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
-public class CalendarFragment extends Fragment {
+public class CalendarFragment extends AppCompatActivity {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_calendar);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.ic_calendar);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.ic_dashboard:
+                        startActivity(new Intent(getApplicationContext(), DashboardFragment.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.ic_calendar:
+                        return true;
+
+                    case R.id.ic_settings:
+                        startActivity(new Intent(getApplicationContext(), SettingsFragment.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                }
+                return false;
+            }
+        });
     }
 }
