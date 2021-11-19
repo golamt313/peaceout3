e3c4cc2a87cc53ef41fcedc9eaa77ebb68fb8f51
JosephThomas2001 <91638891+JosephThomas2001@users.noreply.github.com>
2021-11-19 13:30:22 -0500

    edit to merge

--- App/app/src/main/java/com/example/application/fragments/DashboardFragment.java
+++ App/app/src/main/java/com/example/application/fragments/DashboardFragment.java
@@ -5,20 +5,14 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.application.Home;
 import com.example.application.Login;
 import com.example.application.R;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
