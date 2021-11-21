c26184e4fd6740f0fa1eab93d550ec092925284b
GolamT313 <gj4559@wayne.edu>
2021-11-20 20:17:41 -0500

    Revert "Merge pull request #6 from mtahsin95/bugfix/fixedbuild"
    This reverts commit 2e959871fd2efb4573f12ef9ab16da32520a9f3a, reversing
    changes made to e728de81bcbc62e1f5cda4ee4386f7f045ca0572.

--- App/src/androidTest/java/com/example/application/UnitTestSuite.java
+++
@@ -1,9 +0,0 @@
-package com.example.application;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-// Runs all unit tests.
-@RunWith(Suite.class)
-@Suite.SuiteClasses(value = {ExampleInstrumentedTest.class})
-public class UnitTestSuite {}
--- App/src/test/java/com/example/application/UnitTestSuite.java
+++
@@ -1,8 +0,0 @@
-package com.example.application;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses(value = {ExampleUnitTest.class})
-public class UnitTestSuite {}
