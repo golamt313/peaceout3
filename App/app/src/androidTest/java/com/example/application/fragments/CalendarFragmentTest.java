d74f6e01883f25daebc890e46ed33a09f9106452
juizohora <fv2279@wayne.edu>
2021-12-10 17:25:18 -0500

    CalendarTesting

--- App/app/src/androidTest/java/com/example/application/fragments/CalendarFragmentTest.java
+++ App/app/src/androidTest/java/com/example/application/fragments/CalendarFragmentTest.java
@@ -3,26 +3,37 @@ package com.example.application.fragments;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import android.content.Intent;
 
 import androidx.lifecycle.Lifecycle;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.core.app.ActivityScenario;
 
 import com.example.application.R;
 import com.example.application.b_nav;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class CalendarFragmentTest {
 
     @Test
     public void Test_Events()
     { CalendarFragment calendar = new CalendarFragment();
-      Intent calendarInt = new Intent(calendar,EventEditActivity.class);
-      assertEquals(calendar.startNextMatchingActivity(calendarInt),EventEditActivity.class );
+      Intent calendarInt = new Intent(calendar,WeekViewActivity.class);
+      CalendarViewHolder viewHolder = mock(CalendarViewHolder.class);
+      verify(viewHolder);
+      //assertEquals(calendar.startNextMatchingActivity(calendarInt),EventEditActivity.class );
 
+        try(ActivityScenario<CalendarFragment> CalendarScenario = ActivityScenario.launch(CalendarFragment.class)) {
+            onView(withId(R.id.cellDayText)).perform(click());
+            //assertNotEquals(CalendarScenario.getState(), Lifecycle.State.CREATED);
+        }
     }
 
 
