a3462536a128e03b81bafafafca2259cbdfdb92a
juizohora <fv2279@wayne.edu>
2021-12-10 19:39:18 -0500

    FragmentsAndroidTests

--- App/app/src/androidTest/java/com/example/application/fragments/CalendarFragmentTest.java
+++ App/app/src/androidTest/java/com/example/application/fragments/CalendarFragmentTest.java
@@ -2,9 +2,12 @@ package com.example.application.fragments;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.matches;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -23,16 +26,45 @@ import org.mockito.Mockito;
 public class CalendarFragmentTest {
 
     @Test
-    public void Test_Events()
-    { CalendarFragment calendar = new CalendarFragment();
-      Intent calendarInt = new Intent(calendar,WeekViewActivity.class);
-      CalendarViewHolder viewHolder = mock(CalendarViewHolder.class);
-      verify(viewHolder);
-      //assertEquals(calendar.startNextMatchingActivity(calendarInt),EventEditActivity.class );
+    public void Test_Weekly_Events()
+    {
 
-        try(ActivityScenario<CalendarFragment> CalendarScenario = ActivityScenario.launch(CalendarFragment.class)) {
-            onView(withId(R.id.cellDayText)).perform(click());
-            //assertNotEquals(CalendarScenario.getState(), Lifecycle.State.CREATED);
+        try(ActivityScenario<b_nav> CalendarScenario = ActivityScenario.launch(b_nav.class)) {
+            onView(withId(R.id.ic_calendar)).perform(click());
+            onView(withText("Weekly")).perform(click());
+            onView(withText("New Event")).perform(click());
+            onView(withText("Save")).perform(click());
+            assertEquals(CalendarScenario.getState(), Lifecycle.State.CREATED);
+        }
+    }
+    @Test
+    public void Test_NextMonth_Events()
+    {
+
+        try(ActivityScenario<b_nav> CalendarScenario = ActivityScenario.launch(b_nav.class)) {
+            onView(withId(R.id.ic_calendar)).perform(click());
+            onView(withText("->")).perform(click());
+            assertEquals(CalendarScenario.getState(), Lifecycle.State.CREATED);
+        }
+    }
+    @Test
+    public void Test_PrevMonth_Events()
+    {
+
+        try(ActivityScenario<b_nav> CalendarScenario = ActivityScenario.launch(b_nav.class)) {
+            onView(withId(R.id.ic_calendar)).perform(click());
+            onView(withText("<-")).perform(click());
+            assertEquals(CalendarScenario.getState(), Lifecycle.State.CREATED);
+        }
+    }
+    @Test
+    public void Test_Date_Events()
+    {
+
+        try(ActivityScenario<b_nav> CalendarScenario = ActivityScenario.launch(b_nav.class)) {
+            onView(withId(R.id.ic_calendar)).perform(click());
+            onView(withText("5")).perform(click());
+            assertEquals(CalendarScenario.getState(), Lifecycle.State.CREATED);
         }
     }
 
